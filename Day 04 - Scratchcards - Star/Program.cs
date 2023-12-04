627ee3dcadcb5627c892e799c2ebc3037daead42
Lukas Kolafa <lukas.kolafa@arvato-scs.com>
2023-12-04 10:49:04 +0100

    Day 4

---
+++ Day 04 - Scratchcards - Star/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+bool test = false;
+
+string[] allLines = File.ReadAllLines(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+Regex splitter = new Regex(@"^Card\ +(?<id>\d+):(?<rnd>[0-9\ ]+)\|(?<win>[0-9\ ]+)$", RegexOptions.Compiled);
+
+int[] countCards = new int[allLines.Length];
+for (int i = 0; i < allLines.Length; i++)
+{
+    countCards[i] = 1;
+}
+
+for (int i = 0; i < allLines.Length; i++)
+{
+    var line = allLines[i];
+
+    var split = splitter.Match(line);
+
+    var rnd = split.Groups["rnd"].Value.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    var win = split.Groups["win"].Value.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    var intersect = rnd.Intersect(win).ToArray();
+
+    for (int y = 1; y <= intersect.Length; y++)
+    {
+        countCards[i + y] += countCards[i];
+    }
+}
+
+for (int cnt = 0; cnt < countCards.Length; cnt++)
+{
+    Console.WriteLine("Card: " + (cnt + 1) + " - " + countCards[cnt]);
+}
+
+Console.WriteLine("-----");
+
+Console.WriteLine(countCards.Sum());
---
+++ Day 04 - Scratchcards/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+bool test = false;
+
+string[] allLines = File.ReadAllLines(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+Regex splitter = new Regex(@"^Card\ +(?<id>\d+):(?<rnd>[0-9\ ]+)\|(?<win>[0-9\ ]+)$", RegexOptions.Compiled);
+
+double result = 0;
+
+for (int i = 0; i < allLines.Length; i++)
+{
+    var line = allLines[i];
+
+    var split = splitter.Match(line);
+
+    var id = int.Parse(split.Groups["id"].Value);
+    var rnd = split.Groups["rnd"].Value.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    var win = split.Groups["win"].Value.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    var intersect = rnd.Intersect(win).ToArray();
+
+    if (intersect.Length > 0)
+    {
+        var points = Math.Pow(2, (intersect.Length - 1));
+
+        result += points;
+        Console.WriteLine("POINTS: " + points);
+    }
+
+    Console.WriteLine("ID: " + id);
+    Console.WriteLine("RND: " + string.Join(", ", rnd));
+    Console.WriteLine("WIN: " + string.Join(", ", win));
+}
+
+Console.WriteLine("----");
+Console.WriteLine(result);
+
